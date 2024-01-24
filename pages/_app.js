ed8a559806fd556777fce0c074b03319c1d56469
Hansen Limanta <hansenlimanta@gmail.com>
2024-01-24 23:40:08 +0700

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from "./../components/Layout";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
